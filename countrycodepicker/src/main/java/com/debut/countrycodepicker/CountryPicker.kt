6d8b99692ddb8b6daf72d83c4776511325a5fa2d
Lakhwinder <lakhwinder.singh@debutinfotech.com>
2018-01-09 17:27:46 +0530

    Method name Changed

--- countrycodepicker/src/main/java/com/debut/countrycodepicker/CountryPicker.kt
+++ countrycodepicker/src/main/java/com/debut/countrycodepicker/CountryPicker.kt
@@ -20,7 +20,7 @@ import com.debut.countrycodepicker.listeners.CountryCallBack
  *
  */
 object CountryPicker {
-    fun showCountryPicker(manager: FragmentManager?, callBack: CountryCallBack?) {
+    fun show(manager: FragmentManager?, callBack: CountryCallBack?) {
         if (manager == null || callBack == null) {
             // if callback lsitener or support fragmanet is null then no dialog shown
             throw IllegalArgumentException("No argument can be null")
